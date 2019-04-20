e848cc618dc08e974763fe5833b6f74400167a89
i500896 <madcocomo@gmail.com>
2019-04-20 19:04:49 +0800

    functional refactor. optional

--- src/main/java/com/sap/ase/RomanNumeral.java
+++ src/main/java/com/sap/ase/RomanNumeral.java
@@ -1,6 +1,8 @@
 package com.sap.ase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,13 +11,13 @@ public class RomanNumeral {
     public static RomanNumeral valueOf(int value) {
         List<RomanDigit> digits = new ArrayList<>();
         while (value > 0) {
-            for (RomanDigit digit : RomanDigit.values()) {
-                if (value >= digit.value) {
-                    digits.add(digit);
-                    value -= digit.value;
-                    break;
-                }
-            }
+            int finalValue = value;
+            RomanDigit digit = Arrays.stream(RomanDigit.values())
+                    .sorted(Comparator.comparingInt(RomanDigit::getValue).reversed())
+                    .filter(d -> d.biggerOrEqual(finalValue))
+                    .findFirst().get();
+            digits.add(digit);
+            value -= digit.getValue();
         }
         return new RomanNumeral(digits);
     }
@@ -35,10 +37,21 @@ public class RomanNumeral {
 
     enum RomanDigit {
         M(1000), CM(900), D(500), CD(400), C(100), XC(90), L(50), XL(40), X(10), IX(9), V(5), IV(4), I(1);
+
         private int value;
+
+        public boolean biggerOrEqual(int value) {
+            return this.value <= value;
+        }
+
+        public int getValue() {
+            return value;
+        }
 
         RomanDigit(int value) {
             this.value = value;
         }
+
+
     }
 }
