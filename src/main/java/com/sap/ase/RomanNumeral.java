e154ccbaa6952094fe81237366c34cc2171bf02e
i500896 <madcocomo@gmail.com>
2019-04-20 19:04:42 +0800

    refactor hardcoded II

--- src/main/java/com/sap/ase/RomanNumeral.java
+++ src/main/java/com/sap/ase/RomanNumeral.java
@@ -1,26 +1,33 @@
 package com.sap.ase;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class RomanNumeral {
 
     public static RomanNumeral valueOf(int value) {
-        for (RomanDigit digit : RomanDigit.values()) {
-            if (value == 2) {
-                return new RomanNumeral(RomanDigit.I, RomanDigit.I);
-            }
-            if (digit.value == value) {
-                return new RomanNumeral(digit);
+        List<RomanDigit> digits = new ArrayList<>();
+        while (value > 0) {
+            for (RomanDigit digit : RomanDigit.values()) {
+                if (value > digit.value) {
+                    digits.add(digit);
+                    value -= digit.value;
+                }
+                if (value == digit.value) {
+                    digits.add(digit);
+                    value -= digit.value;
+                }
             }
         }
-        return null;
+        return new RomanNumeral(digits.toArray(new RomanDigit[0]));
     }
 
     private RomanDigit[] digits;
 
-    public RomanNumeral(RomanDigit... value) {
-        this.digits = value;
+    public RomanNumeral(RomanDigit... digits) {
+        this.digits = digits;
     }
 
     @Override
@@ -31,8 +38,7 @@ public class RomanNumeral {
     }
 
     enum RomanDigit {
-        I(1), V(5), X(10), L(50), C(100), D(500), M(1000);
-
+        M(1000), D(500), C(100), L(50), X(10), V(5), I(1);
         private int value;
 
         RomanDigit(int value) {
