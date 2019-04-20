7235285daa76440d08dcf579015d1ac47616bac4
i500896 <madcocomo@gmail.com>
2019-04-20 19:04:31 +0800

    2st failed test

--- src/test/java/com/sap/ase/RomanNumeralTest.java
+++ src/test/java/com/sap/ase/RomanNumeralTest.java
@@ -8,8 +8,14 @@ import org.junit.Test;
 public class RomanNumeralTest {
 
 	@Test
-	public void _1_is_I() throws Exception {
+	public void _1_is_I() {
 	   RomanNumeral roman = RomanNumeral.valueOf(1);
 	   assertThat(roman.toString(), is("I"));
 	}
+
+    @Test
+    public void _5_is_V() {
+        RomanNumeral roman = RomanNumeral.valueOf(5);
+        assertThat(roman.toString(), is("V"));
+    }
 }
