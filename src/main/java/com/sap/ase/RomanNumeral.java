25c8db3fece29063a2d73a92d118ff112b4a5ab6
i500896 <madcocomo@gmail.com>
2019-04-20 19:04:32 +0800

    2nd pass

--- src/main/java/com/sap/ase/RomanNumeral.java
+++ src/main/java/com/sap/ase/RomanNumeral.java
@@ -1,12 +1,21 @@
 package com.sap.ase;
 
 public class RomanNumeral {
+    private int value;
+
     public static RomanNumeral valueOf(int value) {
-        return new RomanNumeral();
+        return new RomanNumeral(value);
+    }
+
+    public RomanNumeral(int value) {
+        this.value = value;
     }
 
     @Override
     public String toString() {
+        if (value == 5) {
+            return "V";
+        }
         return "I";
     }
 }
